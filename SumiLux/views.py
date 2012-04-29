eee2d798a8f260d63ca10f326d8807251f8b3ee1
muriithi <muriithik@gmail.com>
2012-04-29 13:47:10 +0300

    Added css using <style>

--- SumiLux/views.py
+++ SumiLux/views.py
@@ -6,19 +6,41 @@ def sumilux(request):
 				<head>
 					<meta charset = "utf-8"/>
 					<title>Django view to output a html page</title>
+					<style>
+						#wrapper{	width : 90%;
+								margin : 0 auto;
+								}
+						#aside{	width : 30%; 
+								float : right;
+								background-color :#3399FF ;
+								padding : 15px;
+								border-radius : 10px;
+								}
+						#content{
+								width : 60%;
+								float :left;
+								border : 2px blue;
+								background-color : #CC66CC;
+								padding : 15px;
+								border-radius : 10px;
+								}
+						
+					</style>
 				</head>
 
 				<body>
-					<div id = "aside">
-						<p>Lorem ipsum dolor sit amet.</p>
-					</div>
-					<div id = "content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						<p>Fusce placerat auctor mauris, quis condimentum augue dapibus nec.</p>
-						<p>Sed nec orci et lacus elementum malesuada.</p>
-						<p>Donec quis libero a sem vulputate varius.</p>
-						<p>Donec fermentum viverra sem, a porta turpis accumsan in.</p>
-					</div>
+					<div id = "wrapper">
+						<div id = "aside">
+							<p>Lorem ipsum dolor sit amet.</p>
+						</div>
+						<div id = "content">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<p>Fusce placerat auctor mauris, quis condimentum augue dapibus nec.</p>
+							<p>Sed nec orci et lacus elementum malesuada.</p>
+							<p>Donec quis libero a sem vulputate varius.</p>
+							<p>Donec fermentum viverra sem, a porta turpis accumsan in.</p>
+						</div>
+					</div>	
 				</body>
 			</html>
 		"""			
